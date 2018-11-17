0e4c15ab14deba74569d7daff6286f91974d4611
fy <fyshunshun@126.com>
2018-11-17 14:03:08 +0800

    ?

--- src/com/topdomain/ToolButton.java
+++ src/com/topdomain/ToolButton.java
@@ -1,8 +1,25 @@
 package com.topdomain;
 
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
 
 public class ToolButton extends JRadioButton {
+    static {
+        try {
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            parser.parse(ClassLoader.getSystemClassLoader().getResourceAsStream("res/tools.xml"), new DefaultHandler());
+        } catch (SAXException | ParserConfigurationException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public ToolButton(int index) {
 
