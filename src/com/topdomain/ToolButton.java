db40f3c1ed3d651b728ae69170d408e4ecbcea04
fy <fyshunshun@126.com>
2018-11-25 16:31:04 +0800

    ?

--- src/com/topdomain/ToolButton.java
+++ src/com/topdomain/ToolButton.java
@@ -5,6 +5,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.print.attribute.HashPrintJobAttributeSet;
 import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 
 public class ToolButton extends JRadioButton {
     public static int TOOL_SIZE;
+    static HashMap[] tools;
     static {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
@@ -24,13 +26,15 @@ public class ToolButton extends JRadioButton {
             NodeList list = document.getElementsByTagName("tool");
             //System.out.println(list.getLength());
             TOOL_SIZE = list.getLength();
+            tools = new HashMap[TOOL_SIZE];
             for (int i = 0; i < list.getLength(); i++) {
                 Node tool = list.item(i);
                 NodeList child = tool.getChildNodes();
+                tools[i] = new HashMap();
                 for(int j=0; j< child.getLength(); j++){
                     if(child.item(j).getNodeType()==Node.ELEMENT_NODE){
                         System.out.println(child.item(j).getNodeName()+" "+child.item(j).getTextContent());
-                        //tools[i].put(child.item(j).getNodeName(), child.item(j).getTextContent());
+                        tools[i].put(child.item(j).getNodeName(), child.item(j).getTextContent());
                     }
                 }
             }
@@ -40,6 +44,6 @@ public class ToolButton extends JRadioButton {
     }
 
     public ToolButton(int index) {
-
+        setIcon(new ImageIcon(ClassLoader.getSystemResource(tools[index].get("img").toString())));
     }
 }
